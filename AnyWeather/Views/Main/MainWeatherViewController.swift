5fefbdf29d4416a7ec52a61228bde75ca0f4727a
hanulyun <hanulyun88@gmail.com>
2020-06-05 00:59:30 +0900

    # Scroll 로직 구현. 디자인 입혀야 함.

--- AnyWeather/Views/Main/MainWeatherViewController.swift
+++ AnyWeather/Views/Main/MainWeatherViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class MainWeatherViewController: BaseViewController {
     
+    let topview = UIView()
+    let hview = UIView()
     private let tableView: UITableView = UITableView()
+
+    var theight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +29,28 @@ class MainWeatherViewController: BaseViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        
+//        tableView.register(<#T##aClass: AnyClass?##AnyClass?#>, forHeaderFooterViewReuseIdentifier: <#T##String#>)
         
         tableView.register(WeekSummaryTVC.self, forCellReuseIdentifier: WeekSummaryTVC.reuseIdentifer)
     }
     
     override func configureAutolayouts() {
+        view.addSubview(topview)
         view.addSubview(tableView)
         
-        tableView.equalToGuides(guide: self.guide)
+        topview.equalToTop(toAnchor: self.guide.topAnchor)
+        topview.equalToLeading(toAnchor: self.guide.leadingAnchor)
+        topview.equalToTrailing(toAnchor: self.guide.trailingAnchor)
+        if theight == nil {
+            theight = topview.heightAnchor.constraint(equalToConstant: 200)
+            theight.isActive = true
+        }
         
+//        tableView.equalToGuides(guide: self.guide)
+        tableView.equalToTop(toAnchor: topview.bottomAnchor)
+        tableView.equalToLeading(toAnchor: self.guide.leadingAnchor)
+        tableView.equalToTrailing(toAnchor: self.guide.trailingAnchor)
+        tableView.equalToBottom(toAnchor: self.guide.bottomAnchor)
     }
 }
 
@@ -67,9 +83,34 @@ extension MainWeatherViewController: UITableViewDataSource {
 extension MainWeatherViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.section == 0 {
-            return 300
+            return 20
         } else {
             return UITableView.automaticDimension
         }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            hview.backgroundColor = .brown
+            return hview
+        } else {
+            return TimeWeatherView()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 300
+        } else {
+            return 180
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let zeroY = scrollView.contentOffset.y
+        if zeroY < 130 {
+            theight.constant = 200 - zeroY
+        }
+        hview.backgroundColor = UIColor.brown.withAlphaComponent(1 - (zeroY / 300))
     }
 }
