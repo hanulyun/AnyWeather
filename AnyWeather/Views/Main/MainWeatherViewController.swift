6d5f4bd14e94b70e37c289b45cc1954ccde1a8f4
hanulyun <unsanso@naver.com>
2020-06-05 15:58:51 +0900

    # Vertical Scroll 구현

--- AnyWeather/Views/Main/MainWeatherViewController.swift
+++ AnyWeather/Views/Main/MainWeatherViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
+let maxH: CGFloat = 300
+let minH: CGFloat = 100
+
 class MainWeatherViewController: BaseViewController {
     
     let topview = UIView()
-    let hview = UIView()
     private let tableView: UITableView = UITableView()
 
-    var theight: NSLayoutConstraint!
+    var topHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,82 +37,66 @@ class MainWeatherViewController: BaseViewController {
     }
     
     override func configureAutolayouts() {
-        view.addSubview(topview)
         view.addSubview(tableView)
+        view.addSubview(topview)
         
+        tableView.equalToGuides(guide: self.guide)
+        tableView.contentInset = UIEdgeInsets(top: maxH, left: 0, bottom: 0, right: 0)
+        
+        topview.backgroundColor = .purple
         topview.equalToTop(toAnchor: self.guide.topAnchor)
         topview.equalToLeading(toAnchor: self.guide.leadingAnchor)
         topview.equalToTrailing(toAnchor: self.guide.trailingAnchor)
-        if theight == nil {
-            theight = topview.heightAnchor.constraint(equalToConstant: 200)
-            theight.isActive = true
+        if topHeight == nil {
+            topHeight = topview.heightAnchor.constraint(equalToConstant: maxH)
+            topHeight.isActive = true
         }
         
-//        tableView.equalToGuides(guide: self.guide)
-        tableView.equalToTop(toAnchor: topview.bottomAnchor)
-        tableView.equalToLeading(toAnchor: self.guide.leadingAnchor)
-        tableView.equalToTrailing(toAnchor: self.guide.trailingAnchor)
-        tableView.equalToBottom(toAnchor: self.guide.bottomAnchor)
+        view.layoutIfNeeded()
     }
 }
 
 extension MainWeatherViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
             return 25
-        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = UITableViewCell()
-            cell.backgroundColor = .purple
-            return cell
-        } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: WeekSummaryTVC.reuseIdentifer, for: indexPath) as? WeekSummaryTVC else { fatalError("Fail") }
             cell.setData()
             return cell
-        }
     }
 }
 
 extension MainWeatherViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return 20
-        } else {
             return UITableView.automaticDimension
-        }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        if section == 0 {
-            hview.backgroundColor = .brown
-            return hview
-        } else {
             return TimeWeatherView()
-        }
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if section == 0 {
-            return 300
-        } else {
             return 180
-        }
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let zeroY = scrollView.contentOffset.y
-        if zeroY < 130 {
-            theight.constant = 200 - zeroY
+        let offsetY = scrollView.contentOffset.y
+        let y = maxH - (offsetY + maxH)
+        let height = min(max(y, minH), maxH * 1.5)
+        topHeight.constant = height
+        
+        if offsetY < -maxH {
+            scrollView.contentInset = UIEdgeInsets(top: maxH, left: 0, bottom: 0, right: 0)
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: height, left: 0, bottom: 0, right: 0)
         }
-        hview.backgroundColor = UIColor.brown.withAlphaComponent(1 - (zeroY / 300))
+
+        Log.debug("offsetY = \(offsetY)")
     }
 }
