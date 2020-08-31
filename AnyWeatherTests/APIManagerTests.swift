0c7dae4f8bbd242e9ab8e3645a656c3dc755e26b
hanulyun <hanulyun88@gmail.com>
2020-08-31 19:06:26 +0900

    유닛테스트 코드 일부 정리

--- AnyWeatherTests/APIManagerTests.swift
+++ AnyWeatherTests/APIManagerTests.swift
@@ -23,7 +23,7 @@ class APIManagerTests: XCTestCase {
     func testUrlParameters() {
         let mockURLSession: MockURLSession = MockURLSession(data: nil, urlResponse: nil, error: nil)
         apiManager.session = mockURLSession
-    
+        
         apiManager.setComponentUrl(url, param: param).then { url in
             XCTAssertEqual(url.query?.contains(ParamKey.lat.rawValue), true)
             XCTAssertEqual(url.query?.contains(ParamKey.lon.rawValue), true)
@@ -50,9 +50,9 @@ class APIManagerTests: XCTestCase {
             mockModel = model
             modelExpect.fulfill()
         }
-
-        waitForExpectations(timeout: 1) { (error) in
-          XCTAssertNotNil(mockModel)
+        
+        waitForExpectations(timeout: 1) { _ in
+            XCTAssertNotNil(mockModel)
         }
     }
     
@@ -92,31 +92,31 @@ class APIManagerTests: XCTestCase {
             errorRes = error
             errorExpect.fulfill()
         }
-
+        
         waitForExpectations(timeout: 1) { (error) in
             XCTAssertNotNil(errorRes)
             XCTAssertEqual(errorRes as? SessionError, SessionError.failedToParse)
         }
     }
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
